1bd3ffdc3d5b764066b01331b743f9644f641b2d
Jena White <jenale@gmail.com>
2024-10-30 14:13:12 -0400

    added classes

---
+++ ClassExercisePetStore/ClassExercisePetStore/DogLeash.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExercisePetStore
+{
+    internal class DogLeash : Product
+    {
+        public int LengthInches { get; set; }
+        
+        public string Material {  get; set; }
+
+    }
+}
---
+++ ClassExercisePetStore/ClassExercisePetStore/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Press 1 to add a product");
+Console.WriteLine("Type 'exit' to quit");
+
+string userInput = Console.ReadLine();
+
+while (userInput.ToLower() != "exit");
+{
+    userInput = Console.ReadLine();
+    int userInputNumber = Convert.ToInt32(userInput);
+
+    if (userInputNumber == 1)
+    {
+        Console.WriteLine("Come back later");
+    }
+    else
+    {
+        Console.WriteLine("Unvalid input. Please try again");
+        
+    }
+}
